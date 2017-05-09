8f9289bb8ff992254323c4f21b524dd40ef2f4a5
minghaozhi <597575122@qq.com>
2017-05-09 17:32:44 +0800

    11111

--- sheshu_wechat/src/main/java/com/sheshu/wechat/service/impl/MessageServiceImpl.java
+++ sheshu_wechat/src/main/java/com/sheshu/wechat/service/impl/MessageServiceImpl.java
@@ -39,7 +39,7 @@ public class MessageServiceImpl implements MessageService {
         ArrayList<News> newsList = new ArrayList<News>();
         NewsMessage newsMessage = new NewsMessage();
         News news = new News();
-        news.setTitle("董帮辉介绍");
+        news.setTitle("微信公众号测试");
         news.setDescription("");
         news.setPicUrl("http://15h35100r8.iask.in/pic/2.jpg");
         news.setUrl("https://mp.weixin.qq.com/s?__biz=MzI1NjYzMzMwMg==&tempkey=E1h5wEMDCojvpd%2BPpJmtGS09RbDYzSRbloMYvQOOattmkn8V%2F7IkyCALBnBv8U2u6BnoC2nsNIYRNqFwRpT35FtdY%2Bl3tki%2Fqaef5bEfDw5atp0ziv2J%2BaLzOk%2F%2F2Xev%2BH0hmfK%2FpSq06pez%2BPUFAw%3D%3D&chksm=6a22f1bb5d5578adc35244e4d29e8d560eb0fca2f8856d0dc469796e420499bc415c9f7c5970#rd");
